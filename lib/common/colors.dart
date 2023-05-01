495fc5c30721126eacdc52333144712b062672f6
Dante <d.dante03@gmail.com>
2023-05-01 09:32:39 +0200

    added color class

---
+++ lib/common/colors.dart
@@ -0,0 +1,20 @@
+import "package:flutter/material.dart";
+
+class AppColor {
+  static Color primary = const Color.fromARGB(24, 240, 251, 24);
+  static Color secondart = const Color(0xffdd6e41);
+  static Color accent = const Color.fromARGB(255, 250, 158, 184);
+  static Color lightPrimary = const Color(0xff3f3a36);
+  static Color clickedCard = const Color(0xff818181);
+
+  static List<Color> letterColors = [
+    Colors.white,
+    Colors.blue.shade100,
+    Colors.greenAccent,
+    Colors.green,
+    Colors.yellow,
+    Colors.amber,
+    Colors.orange,
+    Colors.red,
+  ];
+}
